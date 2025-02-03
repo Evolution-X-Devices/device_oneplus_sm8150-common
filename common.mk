4f7a437b803debb00e979d0fd6b3a940c52a5de9
Onelots <onelots@onelots.fr>
2025-02-04 00:28:44 +0100

    sm8150: initialize EvolutionX
    sm8150-common: overlay: enable cpu temperature devider and set it to 1000.
    sm8150-common: decommonize A/B OTA post install config
    sm7150-common: Bringup evolution.dependencies
    
    Signed-off-by: Onelots <onelots@onelots.fr>

--- common.mk
+++ common.mk
@@ -7,13 +7,6 @@
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
-# A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
 PRODUCT_PACKAGES += \
     otapreopt_script
 
@@ -234,7 +227,8 @@ $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-evolution
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
